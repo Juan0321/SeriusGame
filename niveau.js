52bb2234b3a41dba7651f9e531691cd4822e4ada
Juan Romero <juancamilo1955@hotmail.com>
2021-03-18 15:19:48 +0100

    presque fini

--- niveau.js
+++ niveau.js
@@ -1,23 +1,28 @@
 var etape=0;
+var point=0;
 
 function suivant(choix) {
     etape++;
     document.body.style.backgroundImage = 'url("image/etape'+etape+'.png")';
-    console.log(choix);
+    console.log(choix=="attaque");
+    console.log(choix)
     if (choix=="cafe"){
         document.getElementById("etape1").style.backgroundImage = 'url("image/cafe.png")';
         document.getElementById("1").style.display = "none";
         document.getElementById("2").style.display = "block ruby";
+        point++;
     }
     if (choix=="the"){
         document.getElementById("etape1").style.backgroundImage = 'url("image/the.png")';
         document.getElementById("1").style.display = "none";
         document.getElementById("2").style.display = "block ruby";
+        point++;
     }
     if (choix=="web"){
         document.getElementById("etape2").style.backgroundImage = 'url("image/web.png")';
         document.getElementById("2").style.display = "none";
         document.getElementById("3").style.display = "block";
+        point+=5;
     }
     if (choix=="mobile"){
         
@@ -40,13 +45,31 @@ function suivant(choix) {
     if(choix=="moyen"){
         document.getElementById("3").style.display = "none";
         document.getElementById("4").style.display = "block ruby";
+        point+=3;
     }
     if(choix=="noir"){
-        document.getElementById("etape4").style.backgroundImage = 'url("image/mobile.png")';
+        document.getElementById("etape4").style.backgroundImage = 'url("image/noir.png")';
         document.getElementById("4").style.display = "none";
+        point+=5;
     }
     if(choix=="sombre"){
-        document.getElementById("etape4").style.backgroundImage = 'url("image/mobile.png")';
+        document.getElementById("etape4").style.backgroundImage = 'url("image/sombre.png")';
         document.getElementById("4").style.display = "none";
+        point+=2;
+    }
+    if(etape==6){
+        document.getElementById("karen").style.display = "block";
+        document.getElementById("perso").style.display = "block";
+        document.getElementById("attaque").style.display = "block";
+        document.getElementById("suivant").style.display = "none";
+    }
+    if(choix=="attaque"){
+        document.getElementById("etape1").style.backgroundImage = "";
+        document.getElementById("etape2").style.backgroundImage = "";
+        document.getElementById("etape3").style.backgroundImage = "";
+        document.getElementById("etape4").style.backgroundImage = "";
+        document.getElementById("attaque").style.display = "none";
+        if(point>=9){document.body.style.backgroundImage = 'url("image/gagne.jpg")';}
+        else{document.body.style.backgroundImage = 'url("image/perdu.jpg")';}
     }
   }
